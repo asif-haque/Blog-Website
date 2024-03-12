39ebc0489d825428449866e174424a489e8bb3e0
Asif <asifhaque0505@gmail.com>
2024-03-12 18:19:31 +0530

    image src updated

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Logo({ width = "100px" }) {
   return (
     <img
-      src="../../public/pexels-dids-1276553.jpg"
+      src="/pexels-dids-1276553.jpg"
       alt="Logo"
       style={{ width, borderRadius: "30px" }}
     />
